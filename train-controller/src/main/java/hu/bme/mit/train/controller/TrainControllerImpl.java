e3707e69449c73e28281eb38fe6df8a3fb07e261
meszbogi <bogi.mesz@gmail.com>
2019-04-01 06:12:03 -0700

    commit

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -4,14 +4,37 @@ import hu.bme.mit.train.interfaces.TrainController;
 import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Table;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 public class TrainControllerImpl implements TrainController {
 
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
+
 	public Table<Long, Integer, Integer> speedLog
 			= HashBasedTable.create();
+
+	public TrainControllerImpl() {
+		starTimer();
+	}
+
+
+	public void starTimer(){
+		Timer timer=new Timer("Timer");
+		TimerTask repeatedTask = new TimerTask() {
+			public void run() {
+				followSpeed();
+			}
+		};
+
+			long delay  = 1000L;
+			long period = 1000L;
+		timer.scheduleAtFixedRate(repeatedTask, delay, period);
+
+	}
 
 	@Override
 	public void followSpeed() {
